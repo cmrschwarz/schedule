36b11fffcd19c7574e7fba11d877ffd3d215dc89
Christian Schwarz <cmrs@cmrs.io>
2021-01-05 16:28:59 +0100

    add --basetime to reduce inaccuracy and --verbose to preview launch time

--- schedule.py
+++ schedule.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+from datetime import datetime
+
+#do imports after aquiring startup time to be a bit more precise
+startup_time = datetime.now().astimezone()
 
 import sys
 import os
 import time
 import dateparser
-from datetime import datetime
 from datetime import timedelta
 import subprocess
 
@@ -13,9 +16,11 @@ def exit_help(code):
         "schedule [-sfph] TIME COMMAND...\n" +
         "    -s:      run synchronuous, don't use a detached process\n" +
         "    -f:      run immediately if time is in the past\n" +
-        "    -p:      keep stdout and stderr for detached process\n" +
+        "    -p:      keep stdout and stderr for execuded command\n" +
         "    -h:      print this help and exit\n" +
-        "    TIME:    3min, or 17:00, etc...\n"+
+        "    -v:      print the scheduled time informally\n" +
+        "    -vv:     print the scheduled time precisely\n" +
+        "    TIME:    3min, 17:00, etc...\n"+
         "    COMMAND: any shell command\n"
     )
     if code == 0:
@@ -25,10 +30,11 @@ def exit_help(code):
     exit(code)
 
 def main():
-    startup_time = datetime.now().astimezone()
+    global startup_time
     synchronous = False
     force = False
     pipe = False
+    verbose = 0
     i = 0
     while True:
         i += 1
@@ -46,6 +52,28 @@ def main():
         if arg in ["-p", "--pipe"]:
             pipe = True
             continue
+        if arg in ["-v", "--verbose"]:
+            verbose += 1
+            if verbose > 2: 
+                sys.stderr.write("max verbosity level is 2\n")
+                exit(1)
+            continue
+        if arg in ["-vv", "--veryverbose"]:
+            verbose = 2
+            continue
+        if arg in ["-b", "--basetime"]:
+            bt=sys.argv[i+1]
+            try:
+                startup_time = dateparser.parse(
+                    bt,
+                    settings={'PREFER_DATES_FROM': 'future', 'RELATIVE_BASE': startup_time}
+                ).astimezone()
+            except:
+                sys.stderr.write("failed to parse basetime from '" + bt + "'\n")
+                exit(1)
+            i += 1
+            continue
+
         schedule_time_str = arg
         cmds = sys.argv[i+1:]
         break
@@ -53,11 +81,25 @@ def main():
     try:
         schedule_time = dateparser.parse(
             schedule_time_str,
-            settings={'PREFER_DATES_FROM': 'future'}
+            settings={'PREFER_DATES_FROM': 'future', 'RELATIVE_BASE': startup_time}
         ).astimezone()
-    except Exception:
+    except:
         sys.stderr.write("failed to parse time from '" + schedule_time_str + "'\n")
         exit(1)
+
+    if verbose == 1:
+        # only import this if we actually need it
+        try:
+            import humanize
+        except:
+            sys.stderr.write("missing python package 'humanize' for -v/--verbose\n")
+            exit(1)
+        if schedule_time > startup_time:
+            print("scheduled time is in " + humanize.naturaldelta(schedule_time - startup_time))
+        else:
+            print("scheduled time was " + humanize.naturaltime(startup_time - schedule_time))
+    elif verbose == 2:
+        print("scheduled time is " + schedule_time.replace(microsecond=0).isoformat())
 
     if not force and schedule_time + timedelta(seconds=3) < startup_time:
         sys.stderr.write("error: scheduled time is in the past\n")
@@ -67,7 +109,7 @@ def main():
         if not pipe:
             devnull = open(os.devnull,"w")
         subprocess.Popen(
-            [os.path.realpath(__file__)] + ["-s", "-f"] + sys.argv[1:],
+            [os.path.realpath(__file__)] + ["-b", str(startup_time.timestamp()), "-s", "-f", schedule_time_str] + cmds,
             start_new_session=True,
             stdout=sys.stdout if pipe else devnull,
             stderr=sys.stderr if pipe else devnull
@@ -75,14 +117,16 @@ def main():
         if not pipe:
             devnull.close()
         exit(0)
-    
-    while schedule_time > datetime.now().astimezone():
-        diff = schedule_time - datetime.now().astimezone()
-        if diff.seconds > 60 * 31:
+    now = datetime.now().astimezone()
+    while schedule_time > now:
+        diff_secs = (schedule_time - now).total_seconds()
+        if diff_secs > 60 * 31:
             time.sleep(60 * 30)
-            continue
-        if diff.seconds == 0: break
-        time.sleep(diff.seconds - 1)
+        elif diff_secs > 0.001:
+            time.sleep(diff_secs)
+        else:
+            break
+        now = datetime.now().astimezone()
             
     subprocess.call(cmds)
 
